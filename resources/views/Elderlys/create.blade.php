9f5405e16f6a6c1044a4866d11a3a60e5435f694
Chinnawatttttttttttttttttt <640112418009@bru.ac.th>
2024-08-22 14:57:11 +0700

    update/create-elderly/02/22/2024/14.57

--- resources/views/Elderlys/create.blade.php
+++ resources/views/Elderlys/create.blade.php
@@ -1,151 +1,211 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container mt-5">
-    <h1 class="mb-4">เพิ่มข้อมูลผู้สูงอายุ</h1>
-
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
+    <div class="card">
+        <div class="card-header">
+            <h2>เพิ่มข้อมูลผู้สูงอายุ</h2>
         </div>
-    @endif
+        <div class="card-body">
+            @if(session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
 
-    @if(session('fail'))
-        <div class="alert alert-danger">
-            {{ session('fail') }}
+            @if(session('fail'))
+                <div class="alert alert-danger">
+                    {{ session('fail') }}
+                </div>
+            @endif
+
+            <form action="{{ route('elderlys.store') }}" method="POST">
+                @csrf
+
+                <div class="row">
+                    <!-- Title -->
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="Title">คำนำหน้า:</label>
+                            <select class="form-control" id="Title" name="Title" required>
+                                <option value="">เลือกคำนำหน้า</option>
+                                <option value="นาย" {{ old('Title') == 'นาย' ? 'selected' : '' }}>นาย</option>
+                                <option value="นาง" {{ old('Title') == 'นาง' ? 'selected' : '' }}>นาง</option>
+                                <option value="นางสาว" {{ old('Title') == 'นางสาว' ? 'selected' : '' }}>นางสาว</option>
+                            </select>
+                            @error('Title')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- First Name -->
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="FirstName">ชื่อ:</label>
+                            <input type="text" class="form-control" id="FirstName" name="FirstName" value="{{ old('FirstName') }}" required>
+                            @error('FirstName')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Last Name -->
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="LastName">นามสกุล:</label>
+                            <input type="text" class="form-control" id="LastName" name="LastName" value="{{ old('LastName') }}" required>
+                            @error('LastName')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <!-- Nick Name -->
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="NickName">ชื่อเล่น:</label>
+                            <input type="text" class="form-control" id="NickName" name="NickName" value="{{ old('NickName') }}">
+                            @error('NickName')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Birthday -->
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="Birthday">วันเกิด:</label>
+                            <input type="date" class="form-control" id="Birthday" name="Birthday" value="{{ old('Birthday') }}" required>
+                            @error('Birthday')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Age -->
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="Age">อายุ:</label>
+                            <input type="number" class="form-control" id="Age" name="Age" value="{{ old('Age') }}" readonly>
+                            @error('Age')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <!-- House Number -->
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="houseNumber">บ้านเลขที่:</label>
+                            <input type="text" class="form-control" id="houseNumber" name="houseNumber" value="{{ old('houseNumber') }}">
+                        </div>
+                    </div>
+
+                    <!-- Village -->
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="village">หมู่:</label>
+                            <input type="text" class="form-control" id="village" name="village" value="{{ old('village') }}">
+                        </div>
+                    </div>
+
+                    <!-- Subdistrict -->
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="subdistrict">ตำบล:</label>
+                            <input type="text" class="form-control" id="subdistrict" name="subdistrict" value="{{ old('subdistrict') }}">
+                        </div>
+                    </div>
+
+                    <!-- District -->
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="district">อำเภอ:</label>
+                            <input type="text" class="form-control" id="district" name="district" value="{{ old('district') }}">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <!-- Province -->
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="province">จังหวัด:</label>
+                            <input type="text" class="form-control" id="province" name="province" value="{{ old('province') }}">
+                        </div>
+                    </div>
+
+                    <!-- Postal Code -->
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="postalCode">รหัสไปรษณีย์:</label>
+                            <input type="text" class="form-control" id="postalCode" name="postalCode" value="{{ old('postalCode') }}">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Hidden field to store combined address -->
+                <input type="hidden" id="Address" name="Address">
+
+                <div class="row">
+                    <!-- Phone -->
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="Phone">เบอร์โทร:</label>
+                            <input type="text" class="form-control" id="Phone" name="Phone" value="{{ old('Phone') }}">
+                            @error('Phone')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Latitude -->
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="Latitude">ละติจูด:</label>
+                            <input type="number" class="form-control" id="Latitude" name="Latitude" value="{{ old('Latitude') }}" step="0.0000001" readonly>
+                            @error('Latitude')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <!-- Longitude -->
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="Longitude">ลองจิจูด:</label>
+                            <input type="number" class="form-control" id="Longitude" name="Longitude" value="{{ old('Longitude') }}" step="0.0000001" readonly>
+                            @error('Longitude')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Map -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="Map">แผนที่:</label>
+                            <div id="map" style="height: 400px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-md-10 offset-md-2">
+                        <button type="submit" class="btn btn-primary" onclick="combineNameAndAddress()">บันทึก</button>
+                    </div>
+                </div>
+            </form>
         </div>
-    @endif
-
-    <form action="{{ route('elderlys.store') }}" method="POST" >
-        @csrf
-
-        <!-- Title -->
-        <div class="form-group">
-            <label for="Title">คำนำหน้า:</label>
-            <select class="form-control" id="Title" name="Title" required>
-                <option value="">เลือกคำนำหน้า</option>
-                <option value="นาย" {{ old('Title') == 'นาย' ? 'selected' : '' }}>นาย</option>
-                <option value="นาง" {{ old('Title') == 'นาง' ? 'selected' : '' }}>นาง</option>
-                <option value="นางสาว" {{ old('Title') == 'นางสาว' ? 'selected' : '' }}>นางสาว</option>
-            </select>
-            @error('Title')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- First Name -->
-        <div class="form-group">
-            <label for="FirstName">ชื่อ:</label>
-            <input type="text" class="form-control" id="FirstName" name="FirstName" value="{{ old('FirstName') }}" required>
-            @error('FirstName')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- Last Name -->
-        <div class="form-group">
-            <label for="LastName">นามสกุล:</label>
-            <input type="text" class="form-control" id="LastName" name="LastName" value="{{ old('LastName') }}" required>
-            @error('LastName')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- Nick Name -->
-        <div class="form-group">
-            <label for="NickName">ชื่อเล่น:</label>
-            <input type="text" class="form-control" id="NickName" name="NickName" value="{{ old('NickName') }}">
-            @error('NickName')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- Birthday -->
-        <div class="form-group">
-            <label for="Birthday">วันเกิด:</label>
-            <input type="date" class="form-control" id="Birthday" name="Birthday" value="{{ old('Birthday') }}" required>
-            @error('Birthday')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- Age -->
-        <div class="form-group">
-            <label for="Age">อายุ:</label>
-            <input type="number" class="form-control" id="Age" name="Age" value="{{ old('Age') }}" readonly>
-            @error('Age')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- Address -->
-        <div class="form-group">
-            <label for="houseNumber">บ้านเลขที่:</label>
-            <input type="text" class="form-control" id="houseNumber" name="houseNumber">
-        </div>
-
-        <div class="form-group">
-            <label for="village">หมู่:</label>
-            <input type="text" class="form-control" id="village" name="village">
-        </div>
-
-        <div class="form-group">
-            <label for="subdistrict">ตำบล:</label>
-            <input type="text" class="form-control" id="subdistrict" name="subdistrict">
-        </div>
-
-        <div class="form-group">
-            <label for="district">อำเภอ:</label>
-            <input type="text" class="form-control" id="district" name="district">
-        </div>
-
-        <div class="form-group">
-            <label for="province">จังหวัด:</label>
-            <input type="text" class="form-control" id="province" name="province">
-        </div>
-
-        <div class="form-group">
-            <label for="postalCode">รหัสไปรษณีย์:</label>
-            <input type="text" class="form-control" id="postalCode" name="postalCode">
-        </div>
-
-        <!-- Hidden field to store combined address -->
-        <input type="hidden" id="Address" name="Address">
-
-        <!-- Phone -->
-        <div class="form-group">
-            <label for="Phone">เบอร์โทร:</label>
-            <input type="text" class="form-control" id="Phone" name="Phone" value="{{ old('Phone') }}">
-            @error('Phone')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- Latitude -->
-        <div class="form-group">
-            <label for="Latitude">ละติจูด:</label>
-            <input type="number" class="form-control" id="Latitude" name="Latitude" value="{{ old('Latitude') }}" step="0.0000001" readonly>
-            @error('Latitude')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- Longitude -->
-        <div class="form-group">
-            <label for="Longitude">ลองจิจูด:</label>
-            <input type="number" class="form-control" id="Longitude" name="Longitude" value="{{ old('Longitude') }}" step="0.0000001" readonly>
-            @error('Longitude')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <!-- Map -->
-        <div class="form-group">
-            <label for="Map">แผนที่:</label>
-            <div id="map" style="height: 400px;"></div>
-        </div>
-        <button type="submit" class="btn btn-primary" onclick="combineNameAndAddress()">บันทึก</button>
-    </form>
+    </div>
 </div>
 
 <!-- Leaflet CSS -->
@@ -164,68 +224,25 @@
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        var marker = L.marker(initialPosition, { draggable: true }).addTo(map);
-
-        map.on('click', function (e) {
-            var clickedLocation = e.latlng;
-            marker.setLatLng(clickedLocation);
-            document.getElementById('Latitude').value = clickedLocation.lat.toFixed(6);
-            document.getElementById('Longitude').value = clickedLocation.lng.toFixed(6);
+        var marker = L.marker(initialPosition).addTo(map);
+        map.on('click', function(e) {
+            marker.setLatLng(e.latlng);
+            document.getElementById('Latitude').value = e.latlng.lat;
+            document.getElementById('Longitude').value = e.latlng.lng;
         });
-
-        marker.on('dragend', function (e) {
-            var draggedLocation = e.target.getLatLng();
-            document.getElementById('Latitude').value = draggedLocation.lat.toFixed(6);
-            document.getElementById('Longitude').value = draggedLocation.lng.toFixed(6);
-        });
-
-        // Update age based on birthday input
-        document.getElementById('Birthday').addEventListener('change', function() {
-            var birthday = new Date(this.value);
-            var age = calculateAge(birthday);
-            document.getElementById('Age').value = age;
-        });
-
-        function calculateAge(birthday) {
-            var today = new Date();
-            var age = today.getFullYear() - birthday.getFullYear();
-            var monthDiff = today.getMonth() - birthday.getMonth();
-            if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthday.getDate())) {
-                age--;
-            }
-            return age;
-        }
     });
 
-     // Function to combine title, first name, and address
-     function combineNameAndAddress() {
-
-        // Get values from individual address fields
+    function combineNameAndAddress() {
         var houseNumber = document.getElementById('houseNumber').value;
-        console.log('1',houseNumber);
         var village = document.getElementById('village').value;
-        console.log('2',village);
         var subdistrict = document.getElementById('subdistrict').value;
-        console.log('3',subdistrict);
         var district = document.getElementById('district').value;
-        console.log('4',district);
         var province = document.getElementById('province').value;
-        console.log('5',province);
         var postalCode = document.getElementById('postalCode').value;
-        console.log('6',postalCode);
 
-        // Combine values into full address only if all fields are filled
-        if (houseNumber || village || subdistrict || district || province || postalCode) {
-            console.log('test')
-            var fullAddress = 'บ้านเลขที่ ' + houseNumber + ' หมู่ ' + village + ' ตำบล ' + subdistrict + ' อำเภอ ' + district + ' จังหวัด ' + province + ' รหัสไปรษณีย์ ' + postalCode;
-            document.getElementById('Address').value = fullAddress;
-        } else {
-            document.getElementById('Address').value = null; // Clear FullAddress if any field is empty
-        }
-    }
-
-    document.forms[0].onsubmit = function() {
-        combineNameAndAddress();
+        var address = `${houseNumber} หมู่ ${village} ตำบล ${subdistrict} อำเภอ ${district} จังหวัด ${province} รหัสไปรษณีย์ ${postalCode}`;
+        document.getElementById('Address').value = address;
     }
 </script>
+
 @endsection
